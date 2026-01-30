cf99b05ec1b14944d353a2bbcb99806478bc4f92
dharmik-pandav <dharmikprajapati777@gmail.com>
2026-01-30 12:29:23 +0530

    Create project fromn fresh

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import "./index.css";
+
+function App() {
+
+  return (
+    <div className="mx-auto h-screen flex justify-center items-center bg-[#191616]">
+      <h1 className="text-white text-3xl font-bold ">Hello Vite + React!</h1>
+
+    </div>
+  );
+}
+
+export default App;
